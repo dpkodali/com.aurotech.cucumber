1cf11947e36c690214d505d470abddca3adafc13
dpkodali <dkodali@gmail.com>
2017-11-17 18:35:46 -0500

    added webdriver node

--- src/test/java/com/aurotech/stepDefinition/LoginTest.java
+++ src/test/java/com/aurotech/stepDefinition/LoginTest.java
@@ -1,9 +1,15 @@
 package com.aurotech.stepDefinition;
 
+import java.net.URL;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -11,12 +17,27 @@ import cucumber.api.java.en.When;
 
 public class LoginTest {
 	
-	WebDriver driver;
+	private WebDriver driver;
+	
+	public void setUpRemote() throws Throwable {
+	
+		
+	DesiredCapabilities cap=DesiredCapabilities.firefox();
+	cap.setPlatform(Platform.XP);
+	URL url=new URL("http://ec2-34-224-214-70.compute-1.amazonaws.com:4444/wd/hub");
+	WebDriver driver=new RemoteWebDriver(url, cap);
+    driver.get("http://ec2-34-227-96-149.compute-1.amazonaws.com:8080/SimpleWebApp/login");
+	
+		
+	}
+	
+	//WebDriver driver;
 	@Given("^Launch Firefox and start application$")
-	public void launch_Firefox_and_start_application() throws Throwable {
-		System.setProperty("webdriver.gecko.driver","D:\\Profiles\\Durga\\Downloads\\geckodriver-v0.19.1-win64\\geckodriver.exe");
-		driver=new FirefoxDriver();
-		driver.manage().window().maximize();
+		public void launch_Firefox_and_start_application() throws Throwable {
+		//System.setProperty("webdriver.gecko.driver","D:\\Profiles\\Durga\\Downloads\\geckodriver-v0.19.1-win64\\geckodriver.exe");
+		//driver=new FirefoxDriver();
+						
+		//driver.manage().window().maximize();
 		driver.get("http://ec2-34-227-96-149.compute-1.amazonaws.com:8080/SimpleWebApp/login");
 		
 	}
